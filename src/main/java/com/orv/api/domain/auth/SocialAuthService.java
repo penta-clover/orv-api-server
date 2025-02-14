16d3ef44708d6e2cca72994daed4ba1aa8aad60a
CaChiJ <chj7239@gmail.com>
2025-02-14 18:00:05 +0900

    fix: 환경변수 설정 에러 해결

--- src/main/java/com/orv/api/domain/auth/SocialAuthService.java
+++ src/main/java/com/orv/api/domain/auth/SocialAuthService.java
@@ -1,7 +1,10 @@
 package com.orv.api.domain.auth;
 
+import org.springframework.stereotype.Service;
+
 import java.util.Map;
 
+@Service
 public interface SocialAuthService {
     Map<String, Object> getUserInfo(String code);
     default String getAuthorizationUrl(String state) {
