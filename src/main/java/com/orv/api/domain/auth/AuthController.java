aa149a90f1d92ca7b2f3514ae1f9040d50bd13c6
CaChiJ <chj7239@gmail.com>
2025-02-14 15:57:35 +0900

    change callback uri

--- src/main/java/com/orv/api/domain/auth/AuthController.java
+++ src/main/java/com/orv/api/domain/auth/AuthController.java
@@ -1,22 +1,26 @@
 package com.orv.api.domain.auth;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v0/auth/")
 public class AuthController {
     @GetMapping("/login/google")
     public void googleLogin(HttpServletResponse response) throws IOException {
-        String clientId = "1058421128761-u2okga6ipnkaesqgt20h5al98l4cdpvm.apps.googleusercontent.com";
-        String redirectUri = "https://orv.rightning.lol/api/v0/auth/callback/google";
+        String clientId = "233929552250-v5jv4q6j2t4mun5hp82eo3kuqp8nmhol.apps.googleusercontent.com";
+        String redirectUri = "https://api.orv.im/api/v0/auth/callback/google";
         String responseType = "code";
         String scope = "openid email profile";
-        String state = "RANDOM_STATE_VALUE"; // CSRF 공격 방지를 위해 랜덤값 사용 권장
+        String state = UUID.randomUUID().toString(); // CSRF 공격 방지를 위해 랜덤값 사용 권장
 
         String googleOAuthUrl = "https://accounts.google.com/o/oauth2/v2/auth"
                 + "?client_id=" + clientId
@@ -29,7 +33,9 @@ public class AuthController {
     }
 
     @GetMapping("/callback/google")
-    public String googleCallback() {
-        return "HI...";
+    public Object googleCallback(HttpServletRequest req) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("url", req.getRequestURL());
+        return map;
     }
 }
