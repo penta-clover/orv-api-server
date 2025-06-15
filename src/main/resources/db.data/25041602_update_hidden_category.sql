31238799414819a30ea9c51f4c62bc7000478085
Hyunjun Choi <chj7239@gmail.com>
2025-06-15 06:13:43 +0000

    Revert "fix: staging server nginx ssl cert file"
    This reverts commit eff6522f86b6aa13f2c138ce17284248ba34ae1d.

--- src/main/resources/db.data/25041602_update_hidden_category.sql
+++
@@ -1,9 +0,0 @@
-BEGIN;
-
-INSERT INTO public.category (id, code)
-VALUES ('c21ea440-1b76-4ac0-a265-31763ff2f08c', 'HIDDEN_3fEMQ3');
-
-INSERT INTO public.category_topic (category_id, topic_id)
-VALUES ('c21ea440-1b76-4ac0-a265-31763ff2f08c', '9b4aefb3-1468-4b56-a9ec-a41ea0ff4e52');
-
-END;
