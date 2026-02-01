7cb81be15e022e135fc662c3029fbb338440774f
CaChiJ <chj7239@gmail.com>
2026-02-01 15:06:10 +0900

    fix: ApiResponse statusCode 필드를 실제 HTTP 응답 코드에 적용한다

---
+++ orv-common/src/main/java/com/orv/common/config/ApiResponseStatusAdvice.java
@@ -0,0 +1,36 @@
+package com.orv.common.config;
+
+import com.orv.common.dto.ApiResponse;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpResponse;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+@RestControllerAdvice
+public class ApiResponseStatusAdvice implements ResponseBodyAdvice<Object> {
+
+    @Override
+    public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
+        return true;
+    }
+
+    @Override
+    public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType,
+                                  Class<? extends HttpMessageConverter<?>> selectedConverterType,
+                                  ServerHttpRequest request, ServerHttpResponse response) {
+        if (body instanceof ApiResponse<?> apiResponse) {
+            try {
+                int statusCode = Integer.parseInt(apiResponse.getStatusCode());
+                if (response instanceof ServletServerHttpResponse servletResponse) {
+                    servletResponse.getServletResponse().setStatus(statusCode);
+                }
+            } catch (NumberFormatException ignored) {
+            }
+        }
+        return body;
+    }
+}
